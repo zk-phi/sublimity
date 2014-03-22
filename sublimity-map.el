85784852b7da87343da3a433f3f200a5c81ac792
zk_phi <zerokay_phi@yahoo.co.jp>
2014-03-22 16:06:33 +0900

    all new sublimity-map with improved performance

--- sublimity-map.el
+++ sublimity-map.el
@@ -18,7 +18,7 @@
 
 ;; Author: zk_phi
 ;; URL: http://hins11.yu-yake.com/
-;; Version: 1.0.8
+;; Version: 2.0.0
 
 ;;; Change Log:
 
@@ -33,155 +33,212 @@
 ;; 1.0.6 add automargin.el workaround
 ;; 1.0.7 add option sublimity-map-keep-commands
 ;; 1.0.8 add option sublimity-map-text-scale
+;; 2.0.0 rewrite almost everything for better performance
 
 ;;; Code:
 
+(require 'cl-lib)
 (require 'sublimity)
-(defconst sublimity-map-version "1.0.6")
+(defconst sublimity-map-version "2.0.0")
 
-;; * customs
+;; + customs
 
-(defcustom sublimity-map-size 15
-  "width of minimap"
+(defcustom sublimity-map-size 17
+  "width of the minimap"
   :group 'sublimity)
 
-(defcustom sublimity-map-fraction 0.3
-  "maximum fraction of minimap width"
+(defcustom sublimity-map-max-fraction 0.3
+  "maximum width that the minimap can get"
   :group 'sublimity)
 
-(defcustom sublimity-map-on-scroll t
-  "if minimap should be automatically displayed on scroll"
+(defcustom sublimity-map-text-scale -8
+  "font rescale for the minimap"
   :group 'sublimity)
 
-(defcustom sublimity-map-on-commands nil
-  "commands after which the minimap should be displayed"
+(defcustom sublimity-map-active-region 'highlight
+  "face for the active region"
   :group 'sublimity)
 
-(defcustom sublimity-map-text-scale -10
-  "size of texts in minimap"
+(defcustom sublimity-map-current-line 'cursor
+  "face for the current line"
   :group 'sublimity)
 
-(defcustom sublimity-map-keep-commands
-  '(previous-line next-line forward-char backward-char
-                  forward-sexp backward-sexp
-                  forward-word backward-word
-                  subword-forward subword-backward
-                  move-end-of-line move-beginning-of-line
-                  back-to-indentation)
-  "commands which should not kill the minimap"
+(defcustom sublimity-map-criteria
+  '((not buffer-read-only)
+    (not (window-minibuffer-p))
+    (derived-mode-p 'prog-mode)
+    (<= (/ sublimity-map-size (window-total-width) 1.0)
+        sublimity-map-max-fraction))
+  "sexps that must be evaluated to non-nil when creating minimap"
   :group 'sublimity)
 
-;; * vars
+(defcustom sublimity-map-setup-hook nil
+  "hook run just after the minimap is activated, with the minimap
+selected."
+  :group 'sublimity)
 
-(defvar sublimity-map-setup-hook nil
-  "hooks that are called just after minimap is activated
-you may assume (selected-window) and (current-buffer) are minimap")
+;; + obsolete variables
 
-;; * map
+(dolist (var '(sublimity-map-on-scroll
+               sublimity-map-on-commands
+               sublimity-map-keep-commands))
+  (eval `(defvar ,var nil))
+  (make-obsolete-variable
+   var "now minimap is updated in post-command-hook."
+   "sublimity-map 2.0.0"))
 
+;; + internal variables
+
+;; global value : the minimap window
 (defvar sublimity-map--window nil)
+
+;; normal buffers : the minimap buffer for this buffer
+;; minimap buffers : the buffer this minimap buffer is for
 (defvar sublimity-map--buffer nil)
-(defvar sublimity-map--overlay nil)
+(make-variable-buffer-local 'sublimity-map--buffer)
 
-(defun sublimity-map--kill ()
-  (when (buffer-live-p sublimity-map--buffer)
-    (kill-buffer sublimity-map--buffer))
+;; variables for minimap bufers
+(defvar sublimity-map--minimap-buffer-p nil)
+(defvar sublimity-map--active-overlay nil)
+(defvar sublimity-map--current-overlay nil)
+(make-variable-buffer-local 'sublimity-map--minimap-buffer-p)
+(make-variable-buffer-local 'sublimity-map--active-overlay)
+(make-variable-buffer-local 'sublimity-map--current-overlay)
+
+;; + create/kill the minimap
+
+(defun sublimity-map--delete-window ()
+  "Kill the minimap window."
   (when (window-live-p sublimity-map--window)
-    (delete-window sublimity-map--window))
-  (when sublimity-map--overlay
-    (setq sublimity-map--overlay nil)))
+    (let* ((partner (window-parameter
+                     sublimity-map--window 'sublimity-map-partner))
+           (margin1 (window-margins partner))
+           (margin2 (window-margins sublimity-map--window))
+           (fringe1 (window-fringes partner))
+           (fringe2 (window-fringes sublimity-map--window)))
+      (delete-window sublimity-map--window)
+      (set-window-margins partner (car margin1) (cdr margin2))
+      (set-window-fringes partner (car fringe1) (cadr fringe2))
+      (setq sublimity-map--window nil))))
 
-(defun sublimity-map--live-p ()
-  (if (and (buffer-live-p sublimity-map--buffer)
-           (window-live-p sublimity-map--window)) t
-    (sublimity-map--kill)
-    nil))
+(defun sublimity-map--split-window ()
+  "Make a minimap window."
+  ;; make sure that the old one is killed
+  (sublimity-map--delete-window)
+  ;; split new one off
+  (let* ((basewin (selected-window))
+         (margin (window-margins basewin))
+         (fringe (window-fringes basewin)))
+    (set-window-margins basewin (car margin) 0)
+    (set-window-fringes basewin (car fringe) 0)
+    (let ((win (split-window
+                basewin (- (+ (or (cdr margin) 0) sublimity-map-size)) t)))
+      (set-window-margins win 0 (cdr margin))
+      (set-window-fringes win 0 (cadr fringe))
+      (set-window-parameter win 'sublimity-map-partner basewin)
+      (setq sublimity-map--window win))))
 
-(defun sublimity-map--update ()
-  (let* ((margins (window-margins))
-         (width (+ (window-width) (or (car margins) 0) (or (cdr margins) 0)))
-         (split-ok (<= (/ sublimity-map-size width 1.0) sublimity-map-fraction)))
-    (unless (window-minibuffer-p)
-      ;; make window
-      (unless (sublimity-map--live-p)
-        (when (setq sublimity-map--window
-                    (when split-ok
-                      (split-window (selected-window) (- sublimity-map-size) t)))
-          (let ((str (buffer-string)))
-            (with-selected-window sublimity-map--window
-              ;; (switch-to-buffer
-              ;;  (setq sublimity-map--buffer (generate-new-buffer "*minimap*")))
-              ;; (insert str)
-              (switch-to-buffer
-               (setq sublimity-map--buffer (clone-indirect-buffer "*minimap*" nil)))
-              (text-scale-set sublimity-map-text-scale)
-              (run-hooks 'sublimity-map-setup-hook)))))
-      ;; update
-      (when (sublimity-map--live-p)
-        (let ((point (point))
-              (beg (window-start))
-              (end (window-end nil t)))
-          (with-selected-window sublimity-map--window
-            (when sublimity-map--overlay
-              (delete-overlay sublimity-map--overlay))
-            (overlay-put (make-overlay beg end) 'face 'highlight)
-            (goto-char point)
-            (recenter)))))))
+(defun sublimity-map--generate-buffer (base)
+  "Make minimap buffer for this buffer."
+  (let ((mlf mode-line-format)
+        (ind (make-indirect-buffer
+              base (concat " *minimap/" (buffer-name base) "*"))))
+    (with-current-buffer ind
+      (setq vertical-scroll-bar              nil
+            truncate-lines                   t
+            buffer-read-only                 t
+            mode-line-format                 (and mode-line-format "")
+            sublimity-map--minimap-buffer-p  t
+            sublimity-map--buffer           base
+            sublimity-map--active-overlay   (make-overlay 0 0)
+            sublimity-map--current-overlay  (make-overlay 0 0))
+      (set (make-local-variable 'auto-hscroll-mode) nil)
+      (overlay-put sublimity-map--active-overlay
+                   'face sublimity-map-active-region)
+      (overlay-put sublimity-map--current-overlay
+                   'face sublimity-map-current-line)
+      (text-scale-set sublimity-map-text-scale)
+      (run-hooks 'sublimity-map-setup-hook))
+    (setq sublimity-map--buffer ind)))
 
-;; * trigger
+(defun sublimity-map--update (basewin miniwin minibuf)
+  "Sync window informations to minimap."
+  (let ((region-beg (window-start basewin))
+        (region-end (window-end basewin t))
+        (point (point)))
+    (with-selected-window miniwin
+      (set-window-buffer nil minibuf t)
+      (with-current-buffer minibuf
+        (goto-char point)
+        (recenter)
+        (move-overlay sublimity-map--active-overlay
+                      region-beg region-end)
+        (move-overlay sublimity-map--current-overlay
+                      (point-at-bol) (line-beginning-position 2))))))
 
-(defvar sublimity-map--delay nil)
-(defvar sublimity-map--timer nil)
+(defun sublimity-map-show ()
+  "Update or create minimap for the current buffer."
+  (interactive)
+  (when (and sublimity-mode
+             (cl-every 'eval sublimity-map-criteria))
+    (unless (and (window-live-p sublimity-map--window)
+                 (eq (window-parent) (window-parent sublimity-map--window)))
+      (sublimity-map--split-window))
+    (unless (buffer-live-p sublimity-map--buffer)
+      (sublimity-map--generate-buffer (current-buffer)))
+    (when (and (window-live-p sublimity-map--window)
+               (buffer-live-p sublimity-map--buffer))
+      (sublimity-map--update
+       (selected-window) sublimity-map--window sublimity-map--buffer))))
 
-(defun sublimity-map-set-delay (secs)
-  "set sublimity-map delay to SECs. if secs is 'inf, disable minimap when idling."
-  (setq sublimity-map--delay secs)
-  (cond ((not sublimity-map--timer)
-         (or (eq secs 'inf)
-             (setq sublimity-map--timer
-                   (run-with-idle-timer secs t 'sublimity-map--idle))))
-        ((eq secs 'inf)
-         (cancel-timer sublimity-map--timer)
-         (setq sublimity-map--timer nil))
+(defun sublimity-map-kill ()
+  "Kill the minimap window."
+  (interactive)
+  (sublimity-map--delete-window))
+
+;; + kill-buffer hook
+
+(defun sublimity-map--kill-buffer-hook ()
+  (when (and (not sublimity-map--minimap-buffer-p)
+             ;; when the buffer being killed is a buffer generated
+             ;; by "with-temp-buffer", its safe to keep minimap active
+             (not (string= (buffer-name) " *temp*")))
+    (when (and (window-live-p sublimity-map--window)
+               (eq (window-parent)
+                   (window-parent sublimity-map--window)))
+      (sublimity-map--delete-window))
+    (when (buffer-live-p sublimity-map--buffer)
+      (kill-buffer sublimity-map--buffer))))
+
+(add-hook 'kill-buffer-hook 'sublimity-map--kill-buffer-hook)
+
+;; + add hooks
+
+(add-hook 'sublimity--pre-command-functions 'sublimity-map--delete-window)
+
+;; kill minimap buffer on "kill-all-local-variables"
+(add-hook 'change-major-mode-hook 'sublimity-map--kill-buffer-hook)
+
+;; + run with timer
+
+(defvar sublimity-map--timer
+  (run-with-idle-timer 0.9 t 'sublimity-map-show))
+
+(defun sublimity-map-set-delay (sec)
+  (cond ((and (memq sec '(nil inf)))
+         (when sublimity-map--timer
+           (cancel-timer sublimity-map--timer))
+         (add-hook 'sublimity--post-command-functions
+                   'sublimity-map-show t))
         (t
-         (timer-set-idle-time sublimity-map--timer secs t))))
+         (remove-hook 'sublimity--post-command-functions
+                      'sublimity-map-show)
+         (when sublimity-map--timer
+           (cancel-timer sublimity-map--timer))
+         (setq sublimity-map--timer
+               (run-with-idle-timer sec t 'sublimity-map-show)))))
 
-(defun sublimity-map--restore-timer ()
-  (when sublimity-map--delay
-    (sublimity-map-set-delay sublimity-map--delay)))
-
-(defun sublimity-map--pre-command ()
-  (unless (member this-command sublimity-map-keep-commands)
-   (sublimity-map--kill)))
-
-(defun sublimity-map--post-command ()
-  (when (member this-command sublimity-map-on-commands)
-    (sublimity-map--update)))
-
-(defun sublimity-map--post-vscroll (_)
-  (when sublimity-map-on-scroll
-    (sublimity-map--update)))
-
-(defun sublimity-map--idle ()
-  (if sublimity-mode
-      (sublimity-map--update)
-    (cancel-timer sublimity-map--timer)
-    (setq sublimity-map--timer nil)))
-
-(add-hook 'sublimity-mode-hook 'sublimity-map--restore-timer)
-(add-hook 'sublimity--post-vscroll-functions 'sublimity-map--post-vscroll)
-(add-hook 'sublimity--pre-command-functions 'sublimity-map--pre-command t)
-(add-hook 'sublimity--post-command-functions 'sublimity-map--post-command)
-
-;; * automargin.el workaround
-
-(eval-after-load 'automargin
-  '(defadvice sublimity-map--update
-     (around sublimity-map-automargin-workaround activate)
-     (flet ((automargin-function () nil))
-       ad-do-it)))
-
-;; * provide
+;; + provide
 
 (provide 'sublimity-map)
 
