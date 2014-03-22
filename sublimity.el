bf1cb5bd5d7da055e980ff9e82db306be5e6bd09
zk_phi <zerokay_phi@yahoo.co.jp>
2014-03-22 16:05:22 +0900

    prepare for new sublimity-map
    now sublimity-mode is a global minor-mode

--- sublimity.el
+++ sublimity.el
@@ -18,7 +18,7 @@
 
 ;; Author: zk_phi
 ;; URL: http://hins11.yu-yake.com/
-;; Version: 1.1.1
+;; Version: 1.1.2
 
 ;;; Commentary:
 
@@ -41,14 +41,17 @@
 ;;; Change Log:
 
 ;; 1.0.0 first released
-;; 1.1.0 turned into minor-mode
-;; 1.1.1 added sublimity-mode-hook
+;; 1.1.0 turn into minor-mode
+;; 1.1.1 add sublimity-mode-hook
+;; 1.1.2 add sublimity-handle-scroll-criteria
+;;       remove add-hook in toplevel
+;;       make sublimity-mode global
 
 ;;; Code:
 
-(defconst sublimity-version "1.1.1")
+(defconst sublimity-version "1.1.2")
 
-;; * customs
+;; + customs
 
 (defgroup sublimity nil
   "smooth-scrolling and minimap, like sublime editor"
@@ -58,61 +61,85 @@
   "hook run when entering sublimity-mode"
   :group 'sublimity)
 
-;; * minor mode
+(defcustom sublimity-handle-scroll-criteria
+  '((eq sublimity--prev-buf (current-buffer))
+    (eq sublimity--prev-wnd (selected-window))
+    (or (not (boundp 'cua--rectangle)) (not cua--rectangle))
+    (or (not (boundp 'multiple-cursors-mode)) (not multiple-cursors-mode))
+    (not (eq major-mode 'shell-mode)))
+  "if any of the sexps evaluates to nil, sublimity does not
+handle scrolling."
+  :group 'sublimity)
 
-(defvar sublimity-auto-hscroll-mode auto-hscroll-mode)
+;; + minor mode
 
+(defvar sublimity-auto-hscroll-mode nil)
+
+;;;###autoload
 (define-minor-mode sublimity-mode
   "smooth-scrolling and minimap, like sublime editor"
   :init-value nil
-  :global nil
-  (if sublimity-mode
-      (progn
-        (setq auto-hscroll-mode nil)
-        (run-hooks 'sublimity-mode-hook))
-    (setq auto-hscroll-mode sublimity-auto-hscroll-mode)))
+  :global t
+  (cond (sublimity-mode
+         (setq sublimity-auto-hscroll-mode auto-hscroll-mode)
+         (setq auto-hscroll-mode nil)
+         (add-hook 'pre-command-hook 'sublimity--pre-command nil)
+         (add-hook 'post-command-hook 'sublimity--post-command t)
+         (add-hook 'window-configuration-change-hook 'sublimity--window-change t)
+         (run-hooks 'sublimity-mode-hook))
+        (t
+         (remove-hook 'pre-command-hook 'sublimity--pre-command)
+         (remove-hook 'post-command-hook 'sublimity--post-command)
+         (remove-hook 'window-configuration-change-hook 'sublimity--window-change t)
+         (setq auto-hscroll-mode sublimity-auto-hscroll-mode))))
 
-(define-globalized-minor-mode sublimity-global-mode
-  sublimity-mode
-  (lambda () (sublimity-mode 1)))
-
-;; * commands (for backward compatibility)
+;; + commands (for backward compatibility)
 
 ;;;###autoload
 (defun sublimity-scroll ()
   (interactive)
   (require 'sublimity-scroll nil t))
+(make-obsolete
+ 'sublimity-scroll "use (require 'sublimity-scroll) instead."
+ "sublimity 1.1.3")
 
 ;;;###autoload
 (defun sublimity-map ()
   (interactive)
   (require 'sublimity-map nil t))
+(make-obsolete
+ 'sublimity-map "use (require 'sublimity-map) instead."
+ "sublimity 1.1.3")
 
-;; * sublimity common vars, functions
+;;;###autoload
+(defun sublimity-global-mode (&optional n)
+  (interactive)
+  (sublimity-mode n))
+(make-obsolete
+ 'sublimity-global-mode "sublimity-mode now affects globally."
+ "sublimity 1.1.3")
 
-(defvar sublimity--pre-command-functions nil
-  "pre-command functions")
+;; + sublimity common vars, functions
 
-(defvar sublimity--post-command-functions nil
-  "like post-command-hook but not called when the buffer or window is switched.")
-
+(defvar sublimity--pre-command-functions nil)
+(defvar sublimity--post-command-functions nil)
+(defvar sublimity--window-change-functions nil)
 (defvar sublimity--post-vscroll-functions nil
   "called with number of lines, when vertical scroll is occurred.")
-
 (defvar sublimity--post-hscroll-functions nil
   "called with number of columns, when horizontal scroll is occurred.")
 
+(defvar sublimity--prepared nil)
 (defvar sublimity--prev-lin (line-number-at-pos (window-start)))
 (defvar sublimity--prev-col (window-hscroll))
 (defvar sublimity--prev-buf (current-buffer))
 (defvar sublimity--prev-wnd (selected-window))
 
-(defun sublimity--should-be-quiet ()
-  (or (not (eq sublimity--prev-buf (current-buffer)))
-      (not (eq sublimity--prev-wnd (selected-window)))
-      (and (boundp 'cua--rectangle) cua--rectangle)
-      (and (boundp 'multiple-cursors-mode) multiple-cursors-mode)
-      (eq major-mode 'shell-mode)))
+(defun sublimity--run-hooks (hook &optional arg)
+  (let* ((sublimity--window-change-functions nil))
+    (if arg
+        (run-hook-with-args 'hook arg)
+      (run-hooks 'hook))))
 
 (defun sublimity--horizontal-recenter ()
   (let ((cols (- (current-column)
@@ -122,55 +149,49 @@
         (scroll-right (- cols))
       (scroll-left cols))))
 
+;; + hook functions
+
 (defun sublimity--pre-command ()
-  (when sublimity-mode
-    (setq sublimity--prev-lin (line-number-at-pos (window-start))
-          sublimity--prev-col (window-hscroll)
-          sublimity--prev-buf (current-buffer)
-          sublimity--prev-wnd (selected-window))
-    (run-hooks 'sublimity--pre-command-functions)))
+  (setq sublimity--prev-lin (line-number-at-pos (window-start))
+        sublimity--prev-col (window-hscroll)
+        sublimity--prev-buf (current-buffer)
+        sublimity--prev-wnd (selected-window)
+        sublimity--prepared t)
+  (sublimity--run-hooks sublimity--pre-command-functions))
 
 (defun sublimity--post-command ()
-  (when (and sublimity-mode
-             (not (sublimity--should-be-quiet)))
-    (let (deactivate-mark)
-      ;; do vscroll
-      (when (or (< (point) (window-start))
-                (>= (point) (window-end)))
-        (recenter))
-      ;; do hscroll
-      (when (and sublimity-auto-hscroll-mode
-                 (or truncate-lines
-                     (truncated-partial-width-window-p))
-                 (or (< (current-column) (window-hscroll))
-                     (< (+ (window-hscroll) (window-width)) (current-column))))
-        (sublimity--horizontal-recenter))
+  ;; avoid running post-command multiple times
+  (when sublimity--prepared
+    (setq sublimity--prepared nil)
+    (let ((handle-scroll (cl-every 'eval sublimity-handle-scroll-criteria)))
+      (when handle-scroll
+        (let (deactivate-mark)
+          ;; do vscroll
+          (when (or (< (point) (window-start))
+                    (>= (point) (window-end)))
+            (recenter))
+          ;; do hscroll
+          (when (and sublimity-auto-hscroll-mode
+                     (or truncate-lines
+                         (truncated-partial-width-window-p))
+                     (or (< (current-column) (window-hscroll))
+                         (< (+ (window-hscroll) (window-width))
+                            (current-column))))
+            (sublimity--horizontal-recenter))))
       ;; call post-command functions
-      (let ((lins (- (line-number-at-pos (window-start))
-                     sublimity--prev-lin))
-            (cols (- (window-hscroll) sublimity--prev-col)))
-        (run-hooks 'sublimity--post-command-functions)
-        (when (not (zerop lins))
-          (run-hook-with-args 'sublimity--post-vscroll-functions lins))
-        (when (not (zerop cols))
-          (run-hook-with-args 'sublimity--post-hscroll-functions cols))))))
+      (sublimity--run-hooks sublimity--post-command-functions)
+      ;; animation
+      (when handle-scroll
+        (let ((lins (- (line-number-at-pos (window-start))
+                       sublimity--prev-lin))
+              (cols (- (window-hscroll) sublimity--prev-col)))
+          (when (not (zerop lins))
+            (sublimity--run-hooks sublimity--post-vscroll-functions lins))
+          (when (not (zerop cols))
+            (sublimity--run-hooks sublimity--post-hscroll-functions cols)))))))
 
-(add-hook 'pre-command-hook 'sublimity--pre-command)
-(add-hook 'post-command-hook 'sublimity--post-command t)
-
-;; * workaround (FIXME)
-
-(defadvice sublimity--pre-command (around sublimity--workaround activate)
-  (condition-case err
-      ad-do-it
-    (error (progn (message "an error occurred in sublimity--pre-command.")
-                  (sit-for 1)))))
-
-(defadvice sublimity--post-command (around sublimity--workaround activate)
-  (condition-case err
-      ad-do-it
-    (error (progn (message "an error occurred in sublimity--post-command.")
-                  (sit-for 1)))))
+(defun sublimity--window-change ()
+  (sublimity--run-hooks sublimity--window-change-functions))
 
 ;; * provide
 
